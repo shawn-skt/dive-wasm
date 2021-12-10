16499587e0c353d6cbb0bb9677884d097ccedc69
Shall <shall@ShalldeMacBook-Pro.local>
2021-12-10 19:49:49 +0800

    first commit

--- hello-wasm/target/debug/deps/hello_wasm.d
+++
@@ -1,5 +0,0 @@
-/Users/shall/VSCode/Rust/hello_world/hello-wasm/target/debug/deps/libhello_wasm.dylib: src/lib.rs
-
-/Users/shall/VSCode/Rust/hello_world/hello-wasm/target/debug/deps/hello_wasm.d: src/lib.rs
-
-src/lib.rs:

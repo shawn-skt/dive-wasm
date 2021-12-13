f2b72e65f12a30f69324ca17d8ec55e85431a391
shawn-skt <2857043555@qq.com>
2021-12-13 19:06:22 +0800

    add liquid

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,8 @@ fn main() -> Result<()> {
           )"#;
     let module = Module::new(&engine, wat)?;
 
-    // All wasm objects operate within the context of a "store". Each
-    // `Store` has a type parameter to store host-specific data, which in
-    // this case we're using `4` for.
     let mut store = Store::new(&engine, 4);
-    // let host_hello = Func::wrap(&mut store, |caller: Caller<'_, u32>, param: i32| {
-    //     println!("Got {} from WebAssembly", param);
-    //     println!("my host state is: {}", caller.data());
-    // });
-
-    // Instantiation of a module requires specifying its imports and then
-    // afterwards we can fetch exports by name, as well as asserting the
-    // type signature of the function with `get_typed_func`.
+    
     let instance = Instance::new(&mut store, &module, &[])?;
     let hello = instance.get_typed_func::<(), f32, _>(&mut store, "add_one")?;
 
